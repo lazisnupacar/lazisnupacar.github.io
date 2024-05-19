2dfdf75bfd0b18f4cad15f46490fea8d590c2f80
ahmadsaputra19 <rebahankan22@gmail.com>
2024-05-19 20:00:15 +0700

    menambahkan file baru

---
+++ script.js
@@ -0,0 +1,68 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const toggleBtnIcon = document.querySelector('.toggle-btn i');
+const dropDownMenu = document.querySelector('.dropdown-menu');
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark' //class toggle yang ada di index.html
+    : 'fa-solid fa-bars' //class toggle yang ada di index.html
+};
+
+// menghilangkan hamburger menu
+
+document.addEventListener('click', function(e){
+    if(!toggleBtn.contains(e.target) && !dropDownMenu.contains(e.target)){
+        dropDownMenu.classList.remove('open');
+        toggleBtnIcon.classList = 'fa-solid fa-bars';
+    };
+});
+
+// header shadow ketika di scroll
+// script.js
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.classList.add('header-shadow');
+    } else {
+        header.classList.remove('header-shadow');
+    }
+});
+
+
+// toTopBtn
+// script.js
+const toTopBtn = document.querySelector('.to-top');
+
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) { // Ubah nilai 100 sesuai dengan posisi scroll yang diinginkan
+        toTopBtn.classList.add('show');
+    } else {
+        toTopBtn.classList.remove('show');
+    }
+});
+
+toTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+//Hubungi Kami
+document.getElementById('whatsapp-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    let whatsappNumber = '6289603170126';
+    let whatsappMessage = `Halo, nama saya ${name}.\nEmail saya: ${email}.\nPesan saya: ${message}`;
+
+    let whatsappURL = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+    
+    window.open(whatsappURL, '_blank');
+});
